7dcb7dfe6395ddf2c72a0e7cff8b95e25831ce31
Robert Beatty <obeatty@gmail.com>
2015-06-03 01:50:17 -0400

    added Net::REST::BufferedReader, which gives a nice read / readLine interface into the channel

---
+++ lib/perl5/Net/REST/BufferedReader.pm
@@ -0,0 +1,68 @@
+package Net::REST::BufferedReader;
+
+use strict;
+
+use Class::MethodMaker [
+    scalar => [ qw( fd buffer chunksize) ],
+    new => [ qw( -init new ) ],
+];
+
+sub init {
+    my ($self, $args) = @_;
+
+    $self->fd($args->{fd});
+
+    $self->chunksize($args->{chunksize} || 8192);
+
+    $self->buffer($args->{buffer} || "");
+}
+
+sub read {
+    my ($self, $args) = @_;
+
+    my $result;
+
+    my $lbuf = length($self->buffer);
+
+    if ($lbuf >= $args->{'length'}) {
+        $result = substr($self->buffer, 0, $args->{'length'});
+    }
+    else {
+        my $tbuffer;
+
+        while ($lbuf < $args->{'length'}) {
+
+            my $rc = sysread($self->fd, $tbuffer, $args->{'length'} - $lbuf);
+
+            $self->buffer($self->buffer . $tbuffer);
+
+            $lbuf += $rc;
+
+            last if $rc < $args->{'length'};
+        }
+
+        $result = substr($self->buffer, 0, $args->{'length'});
+    }
+
+    return $result;
+}
+
+sub readLine {
+
+    my ($self, $args) = @_;
+
+    my $result;
+    
+    if ($self->buffer =~ /^(.+?)(\r?\n)/) {
+        $result = $1;
+        $self->buffer(substr($self->buffer, length($result) + length($2)) );
+    }
+    else {
+        $self->buffer($self->read({ 'length' => $self->chunksize }) );
+        return $self->readLine;
+    }
+
+    return $result;
+}
+
+1;
